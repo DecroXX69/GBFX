ec2a15c79af8306dd9c7f79b5d45fc88c1a9230b
DecroXX <rushikeshpalav23@gmail.com>
2025-01-21 17:20:23 +0530

    new

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,21 @@
 
 import './App.css';
-import Footer from './components/Footer'; import HowItWorks from './minicomponent/Howitworks';
+import Footer from './components/Footer'; 
+import HowItWorks from './minicomponent/Howitworks';
 import Marketspreads from './minicomponent/Marketspreads';
 import Navbar from './components/Navbar';
 import HeroContainer from './minicomponent/HeroContainer';
 import TradingAccounts from './minicomponent/TradingAccounts';
+import Contact from './components/Contact';
 function App() {
   return (
     <div className="App">
       <Navbar />
       <HeroContainer />
       <TradingAccounts />
+      <HowItWorks />
+      <Marketspreads />
+      <Contact />
      <Footer />
     </div>
   );
